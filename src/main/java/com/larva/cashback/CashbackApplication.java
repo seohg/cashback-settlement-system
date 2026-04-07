6ac7e4722d0da893779ef25f5548a736b8197a61
seohg <21900371@handong.ac.kr>
2026-04-07 22:02:22 +0900

    add docker-compose setup

---
+++ src/main/java/com/larva/cashback/CashbackApplication.java
@@ -0,0 +1,13 @@
+package com.larva.cashback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackApplication.class, args);
+	}
+
+}
